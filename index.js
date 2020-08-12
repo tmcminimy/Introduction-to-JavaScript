27de17343df0f54acfb3f89c8af56563fb2bba1f
travis-mcminimy <tmcminimy@gmail.com>
2020-08-11 17:30:38 -0700

    I'm not good at this

--- index.js
+++ index.js
@@ -1,33 +1,49 @@
 /************************************************************** Task 1: Warm-up! **************************************************************/
 //Task a: declare a variable called votingAge, console log true if age > 18 (no function required)
 
-
-
+const votingAge = 18
+const age = 34
+if (age > votingAge){
+console.log(true);    
+}
+else {
+    console.log(false);
+}
 
 
 //Task b: declare a variable and then use a conditional to change the value of that variable based on the value assigned to a second variable (no function required)
 
-
-
+var bestFood = 'Tamale Pie'
+var opinion = Math.round(Math.random())
+if (opinion == 1){
+    bestFood = 'Tamale Pie';
+}
+else if (opinion == 0) {
+    bestFood = 'Sushi';
+}
+console.log(bestFood);
 
 
 //Task c: Convert string ("1999") to integer (1999)  (no function required) // hint look up the Number method
 
-
+Number('1999');
 
 
 
 //Task d: Write a function to multiply a*b 
 
-
-
-
+function multiply(a, b) {
+     return a * b;
+}
+console.log(multiply(100, 100));
 
 /************************************************************** Task 2 **************************************************************/
 //Age in Dog years
 //write a function that takes your age and returns it to you in dog years - they say that 1 human year is equal to seven dog years 
-
-
+function dogYears(a, b) {
+    return a * b;
+}
+console.log(dogYears(34, 7));
 
 
 
@@ -48,7 +64,47 @@
 // 7 - 12 months 4% of their body weight
 
 // when you are finished invoke your function with the weight of 15 lbs and the age of 1 year - if your calculations are correct your result should be 0.44999999999999996
-  
+ 
+function dogFood(result){
+    console.log("You should feed your dog ",result," lbs. of dog food!")
+}
+
+function adult(weight){
+    if (weight <=5){
+        dogFood(weight * 0.5);
+    }
+    else if (weight <= 10){
+        dogFood(weight * .04);
+    }
+    else if (weight <= 15){
+        dogFood(weight * .03);
+    }
+    else {
+        dogFood(weight * .02);
+    }
+}
+
+function puppy(age, weight){
+    if (age >= .166666 && age <= .3333333){
+        dogFood(weight * .1)
+    }
+    else if (age >= .3333333 && age <=.583333){
+        dogFood(weight * .05);
+    }
+    else {
+        dogFood(weight * .04);
+    }
+}
+
+function howMuch(age, weight){
+    if (age <= 1){
+        puppy(age, weight);
+    }
+    else {
+        adult(weight);
+    }
+}
+howMuch(1,15);
 
 
 
@@ -60,17 +116,64 @@
 // use math.random to determine the computers choice 
 // hint while you can complete this with only conditionals based on strings it may help to equate choice to a number 
 
-  
-  
+var computerChoice = Math.random();
+    if (computerChoice <= .34){
+        computerChoice = "Rock";
+    }
+    else if (computerChoice <= .67){
+        computerChoice="Paper";
+    }
+    else {
+        computerChoice = "Scissors";
+    }
+  console.log(computerChoice)
 
+var guess = "Rock"
+function Game(guess){
+    var whole_num = Math.ceil(Math.random() * 3);
+    var compGuess = " ";
+
+    if (whole_num == 1){
+        compGuess = "Rock!"
+    } else if (whole_num == 2){
+        compGuess = "Paper!"
+    } else {
+        compGuess = "Scissors!"
+    }
+    if (guess == "Rock" && compGuess == "Scissors!"){
+        win = true;
+    }else if (guess == "scissors" && compGuess == "Paper!"){
+        win = true;
+    }else if (guess == "paper" && compGuess == "Rock!"){
+        win = true;
+    }else {
+        win = false;
+    }
+    if (win = true){
+        return "You won!";
+    }else {
+        return "You lost!";
+    }
+}
 /************************************************************** Task 5 **************************************************************/
 //Metric Converter
 //a. KM to Miles - should take the number of kilometers and convert it to the equal number of miles
 
 
+<body>
+	<input type='number' placeholder='Enter distance in km' id='km' />
+	<input type='button' onClick='km2mi()' value='Convert' />
+</body>
+<script>
+	var factor = 0.621371
+	var km = document.getElementById('km')
+	var mi
 
-
-
+	function km2mi(){
+		mi.value=(km.value*factor)
+		alert(km.value+' km is equal to '+mi.value+' miles')
+		}
+</script>
 //b. Feet to CM - should take the number of feet and convert it to the equal number of centimeters
   
 
@@ -82,9 +185,23 @@
 // create a function called annoyingSong
 // the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
   
-
-
-
+function annoyingSong() {  
+    var bottles;
+    var bottlesLeft;
+    for (let i = 99; i >= 1; i--) {
+      if (i == 1) {
+        bottles = "bottle";
+        bottlesLeft = "No bottles of soda on the wall!";
+      }else {
+        bottles = "bottles";
+        bottlesLeft = i - 1 + " bottles of soda on the wall!";
+      } console.log(i+ " " + bottles + " of soda on the wall,");
+      console.log(i+ " " + bottles + " of soda,");
+      console.log("Take one down, pass it around,");
+      console.log(bottlesLeft);
+      }      
+  }
+  console.log(annoyingSong());
 
 /************************************************************** Task 7 **************************************************************/
 //Grade Calculator
@@ -95,7 +212,21 @@
 //60s should be D 
 //and anything below 60 should be F
   
-
+function letterGrade () {
+    var grading = Math.round(Math.random() * 100) ;
+    if (grading >= 90 && <=100){
+        console.log("You got an A")
+    }else if (grading >= 80 && <=89){
+        console.log("You got a B")
+    }else if (grading >= 70 && <=79){
+        console.log("You got a C")
+    }else if (grading >= 60 && <=69){
+        console.log("You got a D")
+    }else {
+        console.log("You got an F. Your efforts are about equal to Travis attempting to do javascript")
+    }
+}
+return letterGrade()
   
   
 
